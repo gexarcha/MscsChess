58fdf81454735e021c01210f690b79cf9758306d
Peter Malzacher <peter@lxg0522.gsi.de>
2009-11-26 09:55:08 +0100

    changed tab to 4 blanks

--- Board.h
+++ Board.h
@@ -8,18 +8,18 @@
 
 class Board {
 public:
-	Board();
-	void Init(std::string fenPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
-	bool IsEmpty(int square) const { return board[square] == 0; };
-	bool IsOccupied(int square) const { return !IsEmpty(square); };
-	bool IsSide(Piece::Side s, int square) const { 
-		if(IsEmpty(square)) return false;
-		return board[square]->GetSide() == s;
-	}
-	void Show();
+    Board();
+    void Init(std::string fenPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+    bool IsEmpty(int square) const { return board[square] == 0; };
+    bool IsOccupied(int square) const { return !IsEmpty(square); };
+    bool IsSide(Piece::Side s, int square) const { 
+        if(IsEmpty(square)) return false;
+        return board[square]->GetSide() == s;
+    }
+    void Show();
 private:
-	std::vector<Piece*> piece[2];
-	std::vector<Piece*> board;
+    std::vector<Piece*> piece[2];
+    std::vector<Piece*> board;
 };
 
 #endif
--- Piece.h
+++ Piece.h
@@ -7,83 +7,83 @@ class Board;
 
 class Piece {
 public:
-	enum Side {WHITE, BLACK };
+    enum Side {WHITE, BLACK };
 
-	virtual ~Piece() {};
+    virtual ~Piece() {};
 
-	bool IsWhite() const { return side==WHITE; };
-	bool IsBlack() const { return side==BLACK; };
-	Side GetSide() const { return side; };
-	Side GetOppositeSide() const { return (side == WHITE) ? BLACK : WHITE; }
+    bool IsWhite() const { return side==WHITE; };
+    bool IsBlack() const { return side==BLACK; };
+    Side GetSide() const { return side; };
+    Side GetOppositeSide() const { return (side == WHITE) ? BLACK : WHITE; }
 
-	void Print() const { std::cout << shortName; }
+    void Print() const { std::cout << shortName; }
 
-	virtual bool Attacks(int destination, Board& board) const = 0;
-	
+    virtual bool Attacks(int destination, Board& board) const = 0;
+
 protected:
-	Piece(Side s, int square) : side(s), square(square) {};
+    Piece(Side s, int square) : side(s), square(square) {};
 
-	bool CrawlerAttacks(int destination) const;
-	bool SliderAttacks(int destination, Board& board) const;
+    bool CrawlerAttacks(int destination) const;
+    bool SliderAttacks(int destination, Board& board) const;
 
     Side side;
-	int square;
-	int nRay;
+    int square;
+    int nRay;
     int ray[8];
     char shortName;
 
-	static int mailbox[120];
-	static int mailbox2board[64];
+    static int mailbox[120];
+    static int mailbox2board[64];
 
 private:
 };
 
 class King : public Piece {
 public:
-	King(Side s, int square);	
-	
-	virtual bool Attacks(int destination, Board&) const { return CrawlerAttacks(destination); }
+    King(Side s, int square);	
+
+    virtual bool Attacks(int destination, Board&) const { return CrawlerAttacks(destination); }
 
 };
 
 class Queen : public Piece {	
 public:
-	Queen(Side s, int square);	
-	
-	virtual bool Attacks(int destination, Board& board) const { return SliderAttacks(destination, board); }
+    Queen(Side s, int square);	
+    
+    virtual bool Attacks(int destination, Board& board) const { return SliderAttacks(destination, board); }
 
 };
 
 class Rock : public Piece {
 public:
-	Rock(Side s, int square);	
-	
-	virtual bool Attacks(int destination, Board& board) const { return SliderAttacks(destination, board); }
+    Rock(Side s, int square);	
+
+    virtual bool Attacks(int destination, Board& board) const { return SliderAttacks(destination, board); }
 
 };
 
 class Bishop : public Piece {
 public:
-	Bishop(Side s, int square);	
-	
-	virtual bool Attacks(int destination, Board& board) const { return SliderAttacks(destination, board); }
+    Bishop(Side s, int square);	
+
+    virtual bool Attacks(int destination, Board& board) const { return SliderAttacks(destination, board); }
 
 };
 
 class Knight : public Piece {
 public:
-	Knight(Side s, int square);	
-	
-	virtual bool Attacks(int destination, Board& ) const { return CrawlerAttacks(destination); }
+    Knight(Side s, int square);	
+  
+    virtual bool Attacks(int destination, Board& ) const { return CrawlerAttacks(destination); }
 
 };
 
 
 class Pawn : public Piece {	
 public:
-	Pawn(Side s, int square);	
-	
-	virtual bool Attacks(int destination, Board&) const;
+    Pawn(Side s, int square);	
+
+    virtual bool Attacks(int destination, Board&) const;
 };
 
 
